b27c155a6a4539dfbcbbbf26ec54ffffbd5d7c39
ruslan757 <gimaletdinowruslan@gmail.com>
2026-03-03 18:14:29 +0300

    Add files via upload

--- Nike-studio/js/index.js
+++ Nike-studio/js/index.js
@@ -1,5 +1,6 @@
 import HeaderFixed from "./header.js";
 import BurgerMenu from "./burger.js";
+import Modal from "./modal.js"
 import { productSlider } from "./product-slider.js";
 import { sizes } from "./sizes.js";
 
@@ -28,6 +29,12 @@ try {
     headerFixed
   );
 
+  new Modal({
+    PAGE_BODY: "page__body",
+    PAGE_BODY_NO_SCROLL: "page__body--no-scroll",
+  });
+  
+  
   productSlider();
   sizes();
 } catch (error) {
